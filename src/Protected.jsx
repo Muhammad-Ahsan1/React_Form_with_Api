ba788323a8e2c7232b4c74acfc3da50166e90977
Muhammad-Ahsan1 <ahsanawais786b@gmail.com>
2024-05-29 16:59:28 +0500

    fixing refresh issue

--- src/Protected.jsx
+++ src/Protected.jsx
@@ -1,26 +1,26 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import PropTypes from 'prop-types'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import PropTypes from 'prop-types';
 
+const Protected = ({ Component }) => {
+    const navigate = useNavigate();
 
-const Protected = (props) => {
-    const {Component} = props
-    const navigate = useNavigate()
-
-    useEffect(()=>{
-        let login = localStorage.getItem('login')
-        if(!login){
-            navigate('/login')
+    useEffect(() => {
+        let login = localStorage.getItem('login');
+        if (!login) {
+            navigate('/login');
         }
-    })
+    }, [navigate]);
     
     return (
-    <div>
-        <Component/>
-    </div>
-  )
-}
+        <div>
+            <Component />
+        </div>
+    );
+};
+
 Protected.propTypes = {
-    Component: PropTypes.func
-}
-export default Protected
+    Component: PropTypes.elementType.isRequired
+};
+
+export default Protected;

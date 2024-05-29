8dabffc867a0a750783e4167ed0a6ca1a8d7f794
Muhammad-Ahsan1 <ahsanawais786b@gmail.com>
2024-05-29 16:26:35 +0500

    Completed the Login and Logout Functionality and redirection

---
+++ src/Protected.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import PropTypes from 'prop-types'
+
+
+const Protected = (props) => {
+    const {Component} = props
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        let login = localStorage.getItem('login')
+        if(!login){
+            navigate('/login')
+        }
+    })
+    
+    return (
+    <div>
+        <Component/>
+    </div>
+  )
+}
+Protected.propTypes = {
+    Component: PropTypes.func
+}
+export default Protected
